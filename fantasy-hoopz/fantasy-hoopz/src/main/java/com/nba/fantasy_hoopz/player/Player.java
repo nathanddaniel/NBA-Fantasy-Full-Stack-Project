5bda002ef4e786d99b91328c8dfceeff0df671b8
nathanddaniel <nathandaniel452@gmail.com>
2024-09-09 12:59:04 -0400

    Adding the player class and creating variables for each row

---
+++ fantasy-hoopz/fantasy-hoopz/src/main/java/com/nba/fantasy_hoopz/player/Player.java
@@ -0,0 +1,41 @@
+package com.nba.fantasy_hoopz.player;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.Table;
+
+@Entity
+@Table(name="player_stats")
+public class Player {
+
+    private String player;
+    private Integer age;
+    private Integer games;
+    private Integer gamesStarted;
+    private Float minutePerGame;
+    private Float fieldGoal;
+    private Float fieldGoalAttempted;
+    private Double fieldGoalPercentage;
+    private Float threePoint;
+    private Float threePointAttempted;
+    private Double threePointPercentage;
+    private Float twoPoint;
+    private Float twoPointAttempted;
+    private Double twoPointPercentage;
+    private Double efficientFieldGoalPercentage;
+    private Float freeThrows;
+    private Float freeThrowsAttempted;
+    private Double freeThrowsPercentage;
+    private Float offensiveRebounds;
+    private Float defensiveRebounds;
+    private Float totalRebounds;
+    private Float assists;
+    private Float steals;
+    private Float blocks;
+    private Float turnovers;
+    private Float personalFouls;
+    private Float pointsPerGame;
+
+
+
+
+}
