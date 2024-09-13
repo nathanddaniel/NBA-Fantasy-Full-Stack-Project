eff9eb1d665467531d9f5d530fe129d9b0e6d2de
nathanddaniel <nathandaniel452@gmail.com>
2024-09-13 15:10:44 -0400

    Fixed the backend so that the local displays data properly

--- fantasy-hoopz/fantasy-hoopz/src/main/java/com/nba/fantasy_hoopz/player/Player.java
+++ fantasy-hoopz/fantasy-hoopz/src/main/java/com/nba/fantasy_hoopz/player/Player.java
@@ -9,51 +9,104 @@ import jakarta.persistence.Table;
 @Table(name="roster")
 public class Player {
     @Id
-    @Column(name = "name", unique = true)
+    @Column(name = "player", unique = true)
     private String player;
+
+    @Column(name = "age")
     private Integer age;
 
+    @Column(name = "games")
     private Integer games;
+
+    @Column(name = "games_started")
     private Integer gamesStarted;
+
+    @Column(name = "minutes_played")
     private Float minutePerGame;
 
+    @Column(name = "field_goals_made")
     private Float fieldGoal;
+
+    @Column(name = "field_goals_attempted")
     private Float fieldGoalAttempted;
+
+    @Column(name = "field_goals_percentage")
     private Double fieldGoalPercentage;
 
+    @Column(name = "three_points_made")
     private Float threePoint;
+
+    @Column(name = "three_points_attempted")
     private Float threePointAttempted;
+
+    @Column(name = "three_points_percentage")
     private Double threePointPercentage;
 
+    @Column(name = "two_points_made")
     private Float twoPoint;
+
+    @Column(name = "two_points_attempted")
     private Float twoPointAttempted;
+
+    @Column(name = "two_points_percentage")
     private Double twoPointPercentage;
 
+    @Column(name = "effective_field_goal")
     private Double efficientFieldGoalPercentage;
+
+    @Column(name = "free_throws")
     private Float freeThrows;
+
+    @Column(name = "free_throws_attempted")
     private Float freeThrowsAttempted;
+
+    @Column(name = "free_throws_percentage")
     private Double freeThrowsPercentage;
 
+    @Column(name = "offensive_rebounds")
     private Float offensiveRebounds;
+
+    @Column(name = "defensive_rebounds")
     private Float defensiveRebounds;
+
+    @Column(name = "total_rebounds")
     private Float totalRebounds;
 
+    @Column(name = "assists")
     private Float assists;
+
+    @Column(name = "steals")
     private Float steals;
+
+    @Column(name = "blocks")
     private Float blocks;
+
+    @Column(name = "turnovers")
     private Float turnovers;
+
+    @Column(name = "personal_fouls")
     private Float personalFouls;
+
+    @Column(name = "points_per_game")
     private Float pointsPerGame;
+
+    @Column(name = "team")
     private String team;
 
+    @Column(name = "position")
+    private String position;
+
+    @Column(name = "nation")
     private String nation;
+
+    @Column(name = "college")
     private String college;
 
     public Player() {
 
     }
 
-    public Player(String player, Integer age, Integer games, Integer gamesStarted, Float minutePerGame, Float fieldGoal, Float fieldGoalAttempted, Double fieldGoalPercentage, Float threePoint, Float threePointAttempted, Double threePointPercentage, Float twoPoint, Float twoPointAttempted, Double twoPointPercentage, Double efficientFieldGoalPercentage, Float freeThrows, Float freeThrowsAttempted, Double freeThrowsPercentage, Float offensiveRebounds, Float defensiveRebounds, Float totalRebounds, Float assists, Float steals, Float blocks, Float turnovers, Float personalFouls, Float pointsPerGame, String team, String nation, String college) {
+    public Player(String player, Integer age, Integer games, Integer gamesStarted, Float minutePerGame, Float fieldGoal, Float fieldGoalAttempted, Double fieldGoalPercentage, Float threePoint, Float threePointAttempted, Double threePointPercentage, Float twoPoint, Float twoPointAttempted, Double twoPointPercentage, Double efficientFieldGoalPercentage, Float freeThrows, Float freeThrowsAttempted, Double freeThrowsPercentage, Float offensiveRebounds, Float defensiveRebounds, Float totalRebounds, Float assists, Float steals, Float blocks, Float turnovers, Float personalFouls, Float pointsPerGame, String team, String position, String nation, String college) {
         this.player = player;
         this.age = age;
         this.games = games;
@@ -82,6 +135,7 @@ public class Player {
         this.personalFouls = personalFouls;
         this.pointsPerGame = pointsPerGame;
         this.team = team;
+        this.position = position;
         this.nation = nation;
         this.college = college;
     }
@@ -305,6 +359,14 @@ public class Player {
     public void setPointsPerGame(Float pointsPerGame) {
         this.pointsPerGame = pointsPerGame;
     }
+    public String getPosition() {
+        return position;
+    }
+
+    public void setPosition(String position) {
+        this.position = position;
+    }
+
     public String getTeam() {
         return team;
     }
