0450d4f252c2eb5311ab4413b61d4f191139b486
nathanddaniel <nathandaniel452@gmail.com>
2024-09-20 14:36:08 -0400

    Fixed the backend so that the local displays data properly

---
+++ fantasy-hoopz/fantasy-hoopz/src/main/java/com/nba/fantasy_hoopz/FantasyHoopzApplication.java
@@ -0,0 +1,13 @@
+package com.nba.fantasy_hoopz;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FantasyHoopzApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FantasyHoopzApplication.class, args);
+	}
+
+}
---
+++ fantasy-hoopz/fantasy-hoopz/src/test/java/com/nba/fantasy_hoopz/FantasyHoopzApplicationTests.java
@@ -0,0 +1,13 @@
+package com.nba.fantasy_hoopz;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FantasyHoopzApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
